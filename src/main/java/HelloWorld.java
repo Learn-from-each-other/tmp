27edbaae57f7028c4463c28804320eeb7033f030
zhangyahui <yahui.zhang@yoyi.com.cn>
2020-11-12 20:14:37 +0800

    create

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author zyh
+ * @create 2020-11-12 20:08
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("lishixingshabi!");
+    }
+}
